29f43b8c7564bedec2ceff7e6481e576017eb547
Olusegun Akintimehin <lordshegz@gmail.com>
2026-04-20 00:32:29 +0100

    .

---
+++ api/prisma/migrations/20260419000000_add_google_auth/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable: make password nullable and add googleId
+ALTER TABLE "User" ALTER COLUMN "password" DROP NOT NULL;
+ALTER TABLE "User" ADD COLUMN "googleId" TEXT;
+
+-- CreateIndex: unique constraint on googleId
+CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
